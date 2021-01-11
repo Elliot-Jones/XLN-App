12858379cb7d3d66fec63b8907c2d55991349de6
ElijahBotts00 <bottomleyelijah@gmail.com>
2021-01-11 14:31:17 +0000

    Added proper logo to splashscreen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo2.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>XLN App</Text>
+      <Image source={logo} style={styles.logo}/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,14 +16,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 24,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    color: '#3e3d40',
-    fontSize: 25,
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold",
     position: 'absolute',
     top: 50,
+  },
+  logo: {
+    position: 'absolute',
+    top: 100
   }
 });
