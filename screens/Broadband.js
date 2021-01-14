61ad4ff24c8b3eabc284a98bb6e4a7cafcd60cd6
Elliot Jones <elliotjones142001@gmail.com>
2021-01-14 10:33:57 +0000

    Split files

---
+++ screens/Broadband.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export function Broadband () {
+    return (
+      <View>
+        <Text>Details Screen1</Text>
+      </View>
+    );
+  }
+  export default Broadband;
---
+++ styles/styles.js
@@ -0,0 +1,27 @@
+import {StyleSheet} from 'react-native'
+
+export const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      padding: 24,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    title: {
+      textAlign: "center",
+      fontSize: 30,
+      fontWeight: "bold",
+      position: 'absolute',
+      top: 50,
+    },
+    body: {
+      textAlign: "center",
+      fontSize: 20,
+      color: "#3e3d40"
+    },
+    logo: {
+      position: 'absolute',
+      top: 100
+    }
+  });
