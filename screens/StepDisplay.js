00fffc7695609a0be8e92466fc805edd48db84ce
Elliot Jones <elliotjones142001@gmail.com>
2021-02-17 17:08:47 +0000

    Added form for step response

--- screens/StepDisplay.js
+++ screens/StepDisplay.js
@@ -4,27 +4,65 @@ import {Image, StyleSheets, Text, View, Button, Alert } from 'react-native';
 import logo from '../assets/logo2.png';
 import t from 'tcomb-form-native';
 import {styles} from '../styles/styles.js';
-
+var InputStr = t.refinement(t.String, function(n){return n;});
+const Form = t.form.Form;
 var j = 0;
-  function DisplayStep(array, i,j){
-    console.log(j);
-    return(
-        <View>
-          <Text>{array[i].Steps[j]}</Text>
-        </View>
-    )
-  }
+const Description = t.struct({
+  name:InputStr
+});
+const test = t.enums({
+  Y: 'Yes',
+  N: 'No'
+});
+const yesNo = t.struct({
+  option: test
+});
 
+const options = {
+  fields: {
+    Description: {
+      hasError: true,
+      label: "Enter your Reference Number",
+      placeholder: 'Customer Reference Number'
+    },
+  }
+}
+
+
+
+
+function DisplayStep(array, i,j){
+  console.log(j);
+  switch(array[i].Steps[j][1]){
+    case 1:
+      return(
+
+        <View>
+        <Text>{array[i].Steps[j][0]}</Text>
+        <Form type={Description} options = {options}/>
+        </View>
+      )
+    case 2:
+      return(
+        <View>
+          <Text>{array[i].Steps[j][0]}</Text>
+          <Form type={yesNo}/>
+        </View>
+      )
+  }
+}
 
   export function StepDisplay ({ route, navigation }) {
-    const {selection} = route.params;
-    faultData = require('../BroadbandFaultData.json');
+    handleSubmit = () => {
+
+    }
+
+    const {selection, faultData} = route.params;
     const [test, setTest] = useState(DisplayStep(faultData,selection,j));
       return (
         <View>
             <Text style = "font-weight: bold">{test}</Text>
-            <Button title='No' onPress = {() => {j++; setTest(DisplayStep(faultData,selection,j));}}/>
-            <Button title='Yes' onPress = {() => {j=0;}}/>
+            <Button title='Next' onPress = {() => {j++; setTest(DisplayStep(faultData,selection,j));}}/>
         </View>
       );
     }
