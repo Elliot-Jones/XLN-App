7377eb8b867a472437d51edc9dbaf70f17778a74
Elliot Jones <elliotjones142001@gmail.com>
2021-02-18 16:19:20 +0000

    Preparing email screen link

--- screens/StepDisplay.js
+++ screens/StepDisplay.js
@@ -4,6 +4,7 @@ import {Image, StyleSheets, Text, View, Button, Alert } from 'react-native';
 import logo from '../assets/logo2.png';
 import t from 'tcomb-form-native';
 import {styles} from '../styles/styles.js';
+
 var InputStr = t.refinement(t.String, function(n){return n;});
 const Form = t.form.Form;
 var j = 0;
@@ -33,40 +34,43 @@ const options = {
 
 function DisplayStep(array, i,j){
   console.log(j);
-  switch(array[i].Steps[j][1]){
-    case 1:
-      return(
-
-        <View>
-        <Text>{array[i].Steps[j][0]}</Text>
-        <Form type={Description} options = {options}/>
-        </View>
-      )
-    case 2:
-      return(
-        <View>
-          <Text>{array[i].Steps[j][0]}</Text>
-          <Form type={yesNo}/>
-        </View>
-      )
-    default:
-      return(
-      <View>
-      <Text>{array[i].Steps[j]}</Text>
-    </View>
-      )
+  
+  if(j > array[i].Steps.length-1){
+      console.log("Here");
   }
-}
-
-  export function StepDisplay ({ route, navigation }) {
-    handleSubmit = () => {
-
+  else{
+    switch(array[i].Steps[j][1]){
+      case 1:
+        return(   
+          <View>
+          <Text>{array[i].Steps[j][0]}</Text>
+          <Form type={Description} options = {options}/>
+          </View>
+        )
+        case 2:
+          return(
+            <View>
+            <Text>{array[i].Steps[j][0]}</Text>
+            <Form type={yesNo} options = {options}/>
+            </View>
+        )
+        default:
+          return(
+            <View>
+        <Text>{array[i].Steps[j]}</Text>
+      </View>
+        )
+      }
     }
-
+  }
+  
+  
+  export function StepDisplay ({ route, navigation }) {
+    
     const {selection, faultData} = route.params;
     const [test, setTest] = useState(DisplayStep(faultData,selection,j));
-      return (
-        <View>
+    return (
+      <View>
             <Text style = "font-weight: bold">{test}</Text>
             <Button title='Next' onPress = {() => {j++; setTest(DisplayStep(faultData,selection,j));}}/>
         </View>
